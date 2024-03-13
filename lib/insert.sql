83c1c98368364f7a88a7e4c78acdc6fbdef4caef
zeid <zeidhassan4@gmail.com>
2024-03-13 12:34:10 +0300

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Mr.Chocolate', 10, 'Male', '#754921', 'Aggressive', TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Rowdy', 25, 'Male', '#754922', 'Territorial', TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Tabitha', 14, 'Female', '#754923', 'Curious', TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Sergent Brown', 8, 'Male', '#754924', 'Nomadic', FALSE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Melissa', 20, 'Female', '#754925', 'Maternal', FALSE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Grinch', 13, 'Male', '#754926', 'Solitary', TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Wendy', 21, 'Female', '#754927', 'Aggressive and territorial', TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(NULL, 18, NULL, NULL, NULL, NULL);
+
